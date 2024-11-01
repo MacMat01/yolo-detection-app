5ec81d1ca38f71781eeb91c57093c4cf3a520fb2
macmat01 <matteo.machella01@gmail.com>
2024-11-01 22:52:15 +0100

    Add new dependencies to environment.yml and setup.py
    Updated both environment.yml and setup.py to include essential new libraries such as pandas, seaborn, matplotlib, and pytorch. This ensures that the project has all necessary dependencies for data handling, visualization, and deep learning capabilities.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='yolo-detection-app', version='1.0.0', license='MIT',
       description='A project for detecting cards using YOLOv8', author='MacMat01, SenseiBonsai2k',
       author_email='matteo.machella01@gmail.com, cristian.marinozzi1@gmail.com',
       url='https://github.com/MacMat01/yolo-detection-app', packages=find_packages(),
-      install_requires=['ultralytics', 'opencv-python-headless',  # py-opencv
-                        'pyzbar'], classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
-                                                'License :: OSI Approved :: MIT License',
-                                                'Programming Language :: Python :: 3.12.3', ], )
+      install_requires=['opendatasets', 'ultralytics', 'opencv-python', 'pandas', 'seaborn', 'matplotlib', 'ipykernel',
+                        'torch', 'torchvision', 'torchaudio', 'imgaug', 'tqdm', 'requests', 'pyzbar'], classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
+                                                                                                                    'License :: OSI Approved :: MIT License',
+                                                                                                                    'Programming Language :: Python :: 3.12.3', ], )
